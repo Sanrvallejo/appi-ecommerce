34ff4372a9fd370bf6f49da9f288cf9435f14e39
Sanrvallejo <zanti-23@hotmail.com>
2024-02-24 00:31:01 -0500

    generateOrderNumber and findAll methods for Orde class

--- src/main/java/com/api/ecommerce/services/IOrderService.java
+++ src/main/java/com/api/ecommerce/services/IOrderService.java
@@ -2,6 +2,9 @@ package com.api.ecommerce.services;
 
 import com.api.ecommerce.entities.Order;
 
+import java.util.List;
+
 public interface IOrderService {
   Order save(Order order);
+  List<Order> findAll();
 }
--- src/main/java/com/api/ecommerce/services/OrderServiceImpl.java
+++ src/main/java/com/api/ecommerce/services/OrderServiceImpl.java
@@ -5,6 +5,9 @@ import com.api.ecommerce.repository.IOrderRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrderServiceImpl implements IOrderService {
 
@@ -14,5 +17,39 @@ public class OrderServiceImpl implements IOrderService {
   @Override
   public Order save(Order order) {
     return orderRespository.save(order);
+  }
+
+  @Override
+  public List<Order> findAll() {
+    return orderRespository.findAll();
+  }
+
+  public  String generateOrderNumber() {
+    int number = 0;
+    String numberText ="";
+
+    List<Order> orders = orderRespository.findAll();
+    List<Integer> numbers = new ArrayList<Integer>();
+
+    orders.stream().forEach(or -> numbers.add(Integer.parseInt(or.getNumber())));
+
+    if (orders.isEmpty()) {
+      number = 1;
+    }else {
+      numbers.stream().max(Integer::compare).get();
+      number++;
+    }
+
+    if(number<10) {
+      numberText = "000000"+String.valueOf(number);
+    } else if (number<100) {
+      numberText = "00000"+String.valueOf(number);
+    } else if (number<1000) {
+      numberText = "0000"+String.valueOf(number);
+    } else if (number<100000) {
+      numberText = "000"+String.valueOf(number);
+    }
+
+    return numberText;
   }
 }
